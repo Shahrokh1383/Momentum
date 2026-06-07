01f506b1143c7dd52feb68299145adb7b3bda163
Shahrokh <shahrokh.hatami2004@gmail.com>
2026-06-07 20:23:54 +0330

    adding proxy setting to service.php

--- back-end/config/services.php
+++ back-end/config/services.php
@@ -35,16 +35,24 @@ return [
         ],
     ],
 
-    'google' => [
+   'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID', ''),
         'client_secret' => env('GOOGLE_CLIENT_SECRET', ''),
         'redirect' => env('GOOGLE_REDIRECT_URI', ''),
+        'guzzle' => [
+            'proxy' => env('HTTP_PROXY'),
+            'verify' => env('APP_ENV') === 'production', // Bypass SSL locally to prevent cURL 60 errors
+        ],
     ],
 
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID', ''),
         'client_secret' => env('GITHUB_CLIENT_SECRET', ''),
         'redirect' => env('GITHUB_REDIRECT_URI', ''),
+        'guzzle' => [
+            'proxy' => env('HTTP_PROXY'),
+            'verify' => env('APP_ENV') === 'production',
+        ],
     ],
 
 ];
